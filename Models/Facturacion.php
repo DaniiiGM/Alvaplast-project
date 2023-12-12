c1f1febeddf0f801804fb5a6df2b565e34bb4358
DaniiiGM <daniel_monteza@hotmail.com>
2023-12-12 03:56:07 -0500

    billing de facturación consultas

--- Models/Facturacion.php
+++ Models/Facturacion.php
@@ -9,6 +9,25 @@ class Facturacion{
         $data = $con->query("exec sp_ListarFacturacionXAlmacen 2");
         return $data->fetchAll(PDO::FETCH_OBJ);
     }
+    public static function getListarFacturacion(){
+        $con = Connection::Conectar();
+        
+        // Consulta preparada para obtener productos filtrados de lo que se desea - Facturacion
+        $factu = $con->prepare("exec sp_ListarFacturacionXAlmacen 2 :NROFACT , :orden , :cliente , :RUC , :personal , :fecha , :monto , :moneda ,:estado");
+        $factu->bindParam(":NROFACT", $NROFACT, PDO::PARAM_INT,25);
+        $factu->bindParam(":orden", $serieventa, PDO::PARAM_INT,10);
+        $factu->bindParam(":cliente", $razon_social, PDO::PARAM_STR,50);
+        $factu->bindParam(":RUC", $ruc, PDO::PARAM_INT,15);
+        $factu->bindParam(":personal", $id_personal, PDO::PARAM_STR,15);
+        $factu->bindParam(":orden", $fecha_movimiento, PDO::PARAM_INT,20);
+        $factu->bindParam(":monto", $Sub_Total, PDO::PARAM_INT,20);
+        $factu->bindParam(":moneda", $id_moneda, PDO::PARAM_INT,20);
+        $factu->bindParam(":estado", $estado, PDO::PARAM_INT,5);
+        $factu->execute();
+        
+        // Se retornan los resultados en formato de objeto.
+        return $factu->fetchAll(PDO::FETCH_OBJ);
+    }
 }
 
 ?>
--- views/modals/billingorderlist.php
+++ views/modals/billingorderlist.php
@@ -23,28 +23,23 @@
             </tr>
         </thead>
         <tbody>
+        <?php
+            require_once('../../Models/Facturacion.php');
+            $factura = Facturacion::getListarFacturacion();
+            foreach($factura as $factu){
+           ?>
             <tr>
-                <td>DOC-001</td>
-                <td>OV-12345</td>
-                <td>Empresa A</td>
-                <td>12345678-9</td>
-                <td>Juan Pérez</td>
-                <td>2023-12-01</td>
-                <td>1500.00</td>
-                <td>Dólares</td>
-                <td>En proceso</td>
+                <td><?=$factu->NROFACT?></td>
+                <td><?=$factu->id_venta.$factu->serieventa?></td>
+                <td><?=$factu->razon_social?></td>
+                <td><?=$factu->ruc?></td>
+                <td><?=$factu->id_personal?></td>
+                <td><?=explode(' ',$factu->fecha_movimiento)[0]?></td>
+                <td><?=$factu->Sub_Total?></td>
+                <td><?=$factu->id_moneda?></td>
+                <td><?=$factu->estado?></td>
             </tr>
-            <tr>
-                <td>DOC-002</td>
-                <td>OV-54321</td>
-                <td>Empresa B</td>
-                <td>98765432-1</td>
-                <td>Maria González</td>
-                <td>2023-11-25</td>
-                <td>2800.00</td>
-                <td>Euros</td>
-                <td>Completado</td>
-            </tr>
+            <?php }?>
         </tbody>
     </table>
 </div>
