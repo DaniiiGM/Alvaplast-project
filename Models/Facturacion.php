ef9b556627c97f9ba5c7213e4ac3a92b65ac7a3e
DaniiiGM <daniel_monteza@hotmail.com>
2023-12-13 19:58:35 -0500

    Update de Busqued de Facturaciones

--- Models/Facturacion.php
+++ Models/Facturacion.php
@@ -9,6 +9,20 @@ class Facturacion{
         $data = $con->query("exec sp_ListarFacturacionXAlmacen 2");
         return $data->fetchAll(PDO::FETCH_OBJ);
     }
+    public static function getFacturacionXAlmacenYFecha(string $fechaIni, string $fechaFin, int $id_almacen): array {
+        $con = Connection::Conectar();
+        $tsmt = $con->prepare("exec sp_ListarFacturacionXAlmacenYFecha :fechaini, :fechafin, :almacen");
+        
+        // Formatear fechas al formato 'YYYY-MM-DD' si no están en este formato
+        $fechaIniFormatted = date("Y-m-d", strtotime($fechaIni - 1));
+        $fechaFinFormatted = date("Y-m-d", strtotime($fechaFin + 1));
+    
+        $tsmt->bindParam(":fechaini", $fechaIniFormatted, PDO::PARAM_STR);
+        $tsmt->bindParam(":fechafin", $fechaFinFormatted, PDO::PARAM_STR);
+        $tsmt->bindParam(":almacen", $id_almacen, PDO::PARAM_INT);
+        
+        $tsmt->execute();
+        return $tsmt->fetchAll(PDO::FETCH_OBJ);
+    }
 }
-
 ?>
--- views/modals/billingorderlist.php
+++ views/modals/billingorderlist.php
@@ -23,17 +23,24 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>DOC-001</td>
-                <td>OV-12345</td>
-                <td>Empresa A</td>
-                <td>12345678-9</td>
-                <td>Juan Pérez</td>
-                <td>2023-12-01</td>
-                <td>1500.00</td>
-                <td>Dólares</td>
-                <td>En proceso</td>
-            </tr>
+            <?php
+                require_once('../../Models/Facturacion.php');
+                $facturas = Facturacion::getFacturacion();
+                foreach($facturas as $factus){
+            ?>
+                <tr>
+                    <td><?='NRO-DOC/'.$factus->documento?></td>
+                    <td><?='OV/'.$factus->serie?></td>
+                    <td><?=$factus->cliente?></td>
+                    <td><?=$factus->ruc.' '.$factus->dni?></td>
+                    <td><?=$factus->nombres.' '.$factus->ap_paterno.' '.$factus->ap_materno?></td>
+                    <td><?=explode(' ',$factus->fecha_movimiento)[0]?></td>
+                    <td><?=$factus->pago_inicial?></td>
+                    <td><?=$factus->moneda?></td>
+                    <td><?=$factus->estado?></td>
+                    <td><?=($factus->estado== "P") ? "Pagado" : "Dueda"?></td> 
+                </tr>
+                <?php }?>
             <tr>
                 <td>DOC-002</td>
                 <td>OV-54321</td>
